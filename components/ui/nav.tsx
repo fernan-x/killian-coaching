bb9be12ccbf577e40805b3fe8d2ddec787804fff
Fabien Fernandes Alves <fwfabou@gmail.com>
2024-08-21 14:35:41 -0400

    feat: review spacing and navbar

--- components/ui/nav.tsx
+++ components/ui/nav.tsx
@@ -3,21 +3,22 @@ import { Button } from "./button";
 import Logo from "@/assets/images/logo-white.png";
 import { Menu, Instagram } from "lucide-react";
 import Link from "next/link";
+import { MAIL_TO } from "@/hooks/useMailTo";
 
 const Nav = () => {
   return (
     <nav
-      className={`fixed flex w-full bg-[#000229]/40 backdrop-blur p-6 items-center gap-6 fixed z-20 top-0 start-0 h-20`}
+      className={`fixed flex w-full bg-[#000229]/40 backdrop-blur p-6 items-center gap-12 z-20 top-0 start-0 h-20`}
     >
-      <div className="flex-1 flex items-center gap-6">
-        <Link href="/">
+      <div className="flex-1 flex items-center gap-12">
+        <Link href="/" className="ml-12">
           <Image src={Logo} alt="Killian coaching logo" height={50} />
         </Link>
-        <ul className="hidden flex-1 justify-end gap-6 md:flex text-[#E7F3FF]">
+        <ul className="hidden flex-1 justify-end gap-12 md:flex text-[#E7F3FF]">
           {/* TODO : Hover animation */}
           <li><Link href="/coaching">Coaching</Link></li>
           <li><Link href="/fit-truck">Fit Truck</Link></li>
-          {/* <li><Link href="/contact">Contact</Link></li> */}
+          <li><Link href={MAIL_TO}>Contact</Link></li>
         </ul>
       </div>
       <Link href="https://www.instagram.com/killian_coaching/" target="_blank" className="hidden md:flex">
--- hooks/useMailTo.ts
+++ hooks/useMailTo.ts
@@ -1,8 +1,10 @@
 import { useCallback } from "react";
 
+export const MAIL_TO = 'mailto:killiancoaching.pro@gmail.com';
+
 export const useMailTo = () => {
     const onMailTo = useCallback(() => {
-        window.location.href = 'mailto:killiancoaching.pro@gmail.com';
+        window.location.href = MAIL_TO;
     }, []);
 
     return onMailTo;
