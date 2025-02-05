cbe8ad0e2dfb0491b8bfadecb2a5ec9249ae2c67
Fabien Fernandes Alves <fwfabou@gmail.com>
2025-02-05 18:01:30 -0500

    feat: update description + SEO texts [#163]

--- app/coaching/page.tsx
+++ app/coaching/page.tsx
@@ -7,7 +7,7 @@ export default function Coaching() {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="max-w-2xl text-center lg:text-left">
           <h2 className="uppercase text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Atteignons vos objectifs ensemble</h2>
-          <p className="mt-6 text-lg leading-8 text-gray-600">Que ce soit à domicile ou à distance, vous avez tous les outils pour atteindre vos objectifs.</p>
+          <p className="mt-6 text-lg leading-8 text-gray-600">Profitez d&apos;un entraînement personnalisé et de qualité, avec un matériel professionnel, directement chez vous. Que vous soyez débutant ou sportif confirmé, je m&apos;adapte à vos objectifs et à votre rythme.</p>
         </div>
 
         <div className="flex flex-col md:flex-row gap-6 items-center mt-16">
